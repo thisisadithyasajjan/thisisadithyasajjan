84436183345f0af3653bc29a4a2918c4f139cee0
thisisadithyasajjan <100368796+thisisadithyasajjan@users.noreply.github.com>
2022-02-24 22:04:42 +0530

    Add files via upload

---
+++ div.java
@@ -0,0 +1,9 @@
+public class div
+{
+	public static void main(String args[])
+	{
+		int a=4, b=2, c=0;
+		c=a/b;
+		System.out.println(c);
+	}
+}
---
+++ mul.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=4, b=6, c=0;
+	c=a*b;
+	printf("The sum is : %d", c);
+	return 0;
+}
